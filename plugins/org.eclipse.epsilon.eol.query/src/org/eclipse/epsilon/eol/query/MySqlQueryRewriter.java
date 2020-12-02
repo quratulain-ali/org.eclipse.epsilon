0a321a827e8e09f6fa75b18448d1227caf15dd7c
Qurat ul ain <quratulain.ali@york.ac.uk>
2020-12-02 21:15:18 +0000

    Added query translation plugin

---
+++ plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/MySqlQueryRewriter.java
@@ -0,0 +1,199 @@
+package org.eclipse.epsilon.eol.query;
+
+import java.util.List;
+
+import org.eclipse.epsilon.common.module.ModuleElement;
+import org.eclipse.epsilon.eol.IEolModule;
+import org.eclipse.epsilon.eol.compile.context.IEolCompilationContext;
+import org.eclipse.epsilon.eol.dom.ExpressionStatement;
+import org.eclipse.epsilon.eol.dom.ForStatement;
+import org.eclipse.epsilon.eol.dom.IfStatement;
+import org.eclipse.epsilon.eol.dom.NameExpression;
+import org.eclipse.epsilon.eol.dom.OperationCallExpression;
+import org.eclipse.epsilon.eol.dom.PropertyCallExpression;
+import org.eclipse.epsilon.eol.dom.Statement;
+import org.eclipse.epsilon.eol.dom.StatementBlock;
+import org.eclipse.epsilon.eol.dom.StringLiteral;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelElementTypeNotFoundException;
+import org.eclipse.epsilon.eol.models.IModel;
+import org.eclipse.epsilon.eol.types.EolModelElementType;
+
+public class MySqlQueryRewriter {
+	
+	String tablename;
+	String features;
+	String conditions;
+	String parameters;
+	String limit;
+	boolean optimisable;
+	boolean injectPrintln = false;
+	
+	public void rewrite(IModel model, IEolModule module, IEolCompilationContext context) {
+
+		List<Statement> statements = module.getMain().getStatements();
+		optimiseStatementBlock(model, module, statements, context);
+	}
+
+	public void optimiseStatementBlock(IModel model, IEolModule module, List<Statement> statements, IEolCompilationContext context) {
+		optimisable = true;
+		injectPrintln = false;
+
+		for (Statement statement : statements) {
+			if (statement instanceof ForStatement) {
+				List<Statement> childStatements = ((ForStatement) statement).getBodyStatementBlock().getStatements();
+				optimiseStatementBlock(model, module, childStatements, context);
+			}
+
+			else if (statement instanceof IfStatement) {
+				StatementBlock thenBlock = ((IfStatement) statement).getThenStatementBlock();
+				if (thenBlock != null) {
+					List<Statement> thenStatements = thenBlock.getStatements();
+					optimiseStatementBlock(model, module, thenStatements, context);
+				}
+				StatementBlock elseBlock = ((IfStatement) statement).getElseStatementBlock();
+				if (elseBlock != null) {
+					List<Statement> elseStatements = ((IfStatement) statement).getElseStatementBlock().getStatements();
+					optimiseStatementBlock(model, module, elseStatements, context);
+				}
+			} else {
+				List<ModuleElement> asts = statement.getChildren();
+				optimiseAST(model, asts, context);
+			}
+		}
+	}
+
+	public void optimiseAST(IModel model, List<ModuleElement> asts, IEolCompilationContext context) {
+		for (ModuleElement ast : asts) {
+
+			NameExpression target = new NameExpression(model.getName());
+			NameExpression operation = new NameExpression("runSql");
+			StringLiteral p = new StringLiteral(translateToSql(model, ast, context));
+
+			if (!p.getValue().equalsIgnoreCase("Not optimisable")) {
+				OperationCallExpression rewritedQuery = new OperationCallExpression(target, operation, p);
+
+				if (injectPrintln) {
+					rewritedQuery = new OperationCallExpression(rewritedQuery, new NameExpression("println"));
+				}
+
+				if (ast.getParent() instanceof ExpressionStatement)
+					((ExpressionStatement) ast.getParent()).setExpression(rewritedQuery);
+
+				else
+					((OperationCallExpression) ast.getParent()).setTargetExpression(rewritedQuery);
+
+				System.out.println("Translated = " + p.getValue());
+			}
+		}
+
+	}
+
+	public String translateToSql(IModel model, ModuleElement ast, IEolCompilationContext context) {
+		tablename = "";
+		features = "";
+		conditions = "";
+		parameters = "";
+		limit = "";
+
+		if (ast instanceof OperationCallExpression
+				&& !(((OperationCallExpression) ast).getTargetExpression() instanceof NameExpression)) {
+
+			for (ModuleElement astChild : ast.getChildren()) {
+				if (astChild instanceof OperationCallExpression)
+					astToSql(model, (OperationCallExpression) astChild, context);
+
+				if (astChild instanceof PropertyCallExpression)
+					astToSql(model, (PropertyCallExpression) astChild, context);
+			}
+			astToSql(model, (OperationCallExpression) ast, context);
+		}
+
+		if (ast instanceof PropertyCallExpression) {
+			if (!(((PropertyCallExpression) ast).getTargetExpression() instanceof NameExpression)) {
+
+				for (ModuleElement astChild : ast.getChildren()) {
+					if (astChild instanceof OperationCallExpression)
+						astToSql(model, (OperationCallExpression) astChild, context);
+
+					if (astChild instanceof PropertyCallExpression)
+						astToSql(model, (PropertyCallExpression) astChild, context);
+				}
+			}
+			astToSql(model, (PropertyCallExpression) ast, context);
+		}
+
+		if (optimisable) {
+			if (tablename.isEmpty())
+				return "Not optimisable";
+			if (conditions.isEmpty() && limit.isEmpty())
+				return "SELECT " + features + " FROM " + tablename;
+
+			else if (conditions.isEmpty())
+				return "SELECT " + features + " FROM " + tablename + " limit " + limit;
+
+			else
+				return "SELECT " + features + " FROM " + tablename + " WHERE " + conditions + " = " + parameters;
+
+		} else
+			return "Not optimisable";
+	}
+
+	public void astToSql(IModel model, OperationCallExpression ast, IEolCompilationContext context) {
+		if (!(ast.getTargetExpression() instanceof NameExpression) || (ast.getChildren() != null)) {
+			for (ModuleElement astChild : ast.getChildren()) {
+				if (astChild instanceof OperationCallExpression)
+					astToSql(model, (OperationCallExpression) astChild, context);
+
+				if (astChild instanceof PropertyCallExpression)
+					astToSql(model, (PropertyCallExpression) astChild, context);
+			}
+			if (ast.getName().equals("size"))
+				features = "COUNT(" + features + ")";
+			if (ast.getName().equals("asSet"))
+				features = "DISTINCT " + features;
+			if (ast.getName().equals("first")) {
+				limit = "1";
+			}
+			if (ast.getName().equals("println")) {
+				injectPrintln = true;
+			}
+		}
+	}
+
+	public void astToSql(IModel model, PropertyCallExpression ast, IEolCompilationContext context) {
+		if (!(ast.getTargetExpression() instanceof NameExpression) || (ast.getChildren() != null)) {
+			for (ModuleElement astChild : ast.getChildren()) {
+				if (astChild instanceof OperationCallExpression)
+					astToSql(model, (OperationCallExpression) astChild, context);
+
+				if (astChild instanceof PropertyCallExpression)
+					astToSql(model, (PropertyCallExpression) astChild, context);
+			}
+			if (ast.getName().equals("all") || ast.getName().equals("allInstances")) {
+				if (ast.getTargetExpression().getResolvedType() instanceof EolModelElementType) {
+
+					IModel m = null;
+					try {
+						m = ((EolModelElementType) ast.getTargetExpression().getResolvedType()).getModel(context);
+					} catch (EolModelElementTypeNotFoundException e) {
+
+						e.printStackTrace();
+					}
+
+					if (m == model) {
+						tablename = ((EolModelElementType) ast.getTargetExpression().getResolvedType()).getTypeName();
+					} else {
+						optimisable = false;
+
+					}
+					features = "*";
+
+				}
+
+			} else {
+				features = ast.getName();
+			}
+		}
+	}
+
+}
--- plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryRewritingView.java
+++ plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryRewritingView.java
@@ -1,14 +1,29 @@
 package org.eclipse.epsilon.eol.query;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.epsilon.common.dt.editor.ModelTypeExtensionFactory;
 import org.eclipse.epsilon.common.dt.util.ListContentProvider;
+import org.eclipse.epsilon.emc.emf.SubEmfModelFactory;
 import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.compile.context.IEolCompilationContext;
+import org.eclipse.epsilon.eol.dom.ModelDeclaration;
+import org.eclipse.epsilon.eol.models.IModel;
+import org.eclipse.epsilon.eol.models.IRewriter;
+import org.eclipse.epsilon.eol.parse.EolUnparser;
 import org.eclipse.epsilon.eol.staticanalyser.EolStaticAnalyser;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IViewSite;
@@ -22,73 +37,103 @@ import org.eclipse.ui.part.ViewPart;
 
 public class QueryRewritingView extends ViewPart {
 	protected IEditorPart editor;
-     ListViewer viewer;
-     
-	 public QueryRewritingView() {
-             super();
-     }
-	 
-	 public void init(IViewSite site) throws PartInitException {
-         super.init(site);
-         // Normally we might do other stuff here.
-	 }
-	 
-	 
+	private Label label;
+	public String text;
+	ListViewer viewer;
+	IEolCompilationContext context;
+
+	public QueryRewritingView() {
+		super();
+	}
+
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+	}
+
 	@Override
 	public void createPartControl(Composite parent) {
-		viewer = new ListViewer(parent,0);
-		viewer.setContentProvider(new ListContentProvider());
+		label = new Label(parent, 0);
 		
-        IToolBarManager toolbar = getViewSite().getActionBars().getToolBarManager();
+		//TODO: remove
+//		viewer = new ListViewer(parent, 0);
+//		viewer.setContentProvider(new ListContentProvider());
+//		viewer.setLabelProvider(new MyLabelProvider());
+       
+		IToolBarManager toolbar = getViewSite().getActionBars().getToolBarManager();
 		toolbar.add(new RefreshAction(this));
-        
-        IWorkbench wb = PlatformUI.getWorkbench();
-        IWorkbenchWindow window = wb.getActiveWorkbenchWindow();
-        IWorkbenchPage page = window.getActivePage();
-        IEditorPart editor = page.getActiveEditor();
-        IEditorInput input = editor.getEditorInput();
-        IPath path = ((FileEditorInput)input).getPath();
-        EolModule module = new EolModule();
+	
+		EolModule module = new EolModule();
+		
 		try {
-			module.parse(new File(path.toString()));
+			module.parse(new File(getActiveEditorFilePath()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		module.compile();
+		
+		context = module.getCompilationContext();
+		
+		context.setModelFactory(new ModelTypeExtensionFactory());
+		
 		new EolStaticAnalyser().validate(module);
-	    viewer.setInput(module.getTranslatedQueries());
+		
+		new QueryRewriter().invokeRewriters(module);
+		
+		label.setText(new EolUnparser().unparse(module));
 	}
-	
+
+	public class MyLabelProvider extends LabelProvider implements IColorProvider {
+
+		public String getText(Object element) {
+			return String.valueOf(element);
+		}
+
+		public Color getForeground(Object element) {
+			Display display = Display.getDefault();
+			return display.getSystemColor(SWT.COLOR_RED);
+		}
+
+		public Color getBackground(Object element) {
+			return null;
+		}
+	}
+
+	@Override
+	public void setFocus() {
+		label.setFocus();
+
+	}
+
 	public IEditorPart getEditor() {
 		return editor;
 	}
 	
-	public void render(IEditorPart editor) {
-		//if (editor != null) {
-			IWorkbench wb = PlatformUI.getWorkbench();
-	        IWorkbenchWindow window = wb.getActiveWorkbenchWindow();
-	        IWorkbenchPage page = window.getActivePage();
-	        editor = page.getActiveEditor();
-	        IEditorInput input = editor.getEditorInput();
-	        IPath path = ((FileEditorInput)input).getPath();
-	        EolModule module = new EolModule();
-			try {
-				module.parse(new File(path.toString()));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		
-		module.compile();
-		new EolStaticAnalyser().validate(module);
-		viewer.setInput(module.getTranslatedQueries());
-		}
-
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-		
+	public String getActiveEditorFilePath() {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow window = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = window.getActivePage();
+		IEditorPart editor = page.getActiveEditor();
+		IEditorInput input = editor.getEditorInput();
+		IPath path = ((FileEditorInput) input).getPath();
+		return path.toString();
 	}
-	
+
+	public void render(IEditorPart editor) {
+		
+		EolModule module = new EolModule();
+		try {
+			module.parse(new File(getActiveEditorFilePath()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		context = module.getCompilationContext();
+		context.setModelFactory(new ModelTypeExtensionFactory());
+		
+		new EolStaticAnalyser().validate(module);
+		
+		new QueryRewriter().invokeRewriters(module);
+
+		label.setText(new EolUnparser().unparse(module));
+	}
 
 }
