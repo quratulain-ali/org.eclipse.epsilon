34c8e83985aa243ffc69cd8dbd2548f89705d7a7
Qurat ul ain <quratulain.ali@york.ac.uk>
2021-01-15 12:27:17 +0000

    Updated emf model testcases

--- plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryTranslationEmfModelTests.java
+++ plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryTranslationEmfModelTests.java
@@ -1,12 +1,15 @@
 package org.eclipse.epsilon.eol.query;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.eclipse.epsilon.emc.emf.SubEmfModelFactory;
+import org.eclipse.epsilon.emc.mysql.SubModelFactory;
 import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.compile.context.EolCompilationContext;
 import org.eclipse.epsilon.eol.dom.ModelDeclaration;
 import org.eclipse.epsilon.eol.models.IModel;
-import org.eclipse.epsilon.eol.models.IRewriter;
 import org.eclipse.epsilon.eol.parse.EolUnparser;
 import org.eclipse.epsilon.eol.staticanalyser.EolStaticAnalyser;
 import org.junit.Test;
@@ -20,54 +23,36 @@ public class QueryTranslationEmfModelTests extends TestCase {
 		EolModule module = new EolModule();
 
 		module.parse(new File("src/org/eclipse/epsilon/eol/query/emfTest.eol"));
-		module.compile();
+EolCompilationContext context = module.getCompilationContext();
 		
-		module.getCompilationContext().setModelFactory(new SubEmfModelFactory());
+		for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
+			if (modelDeclaration.getDriverNameExpression().getName().equals("MySQL")) 
+				context.setModelFactory(new SubModelFactory());
+
+			if (modelDeclaration.getDriverNameExpression().getName().equals("EMF")) 
+				context.setModelFactory(new SubEmfModelFactory());
+		}
 		
 		new EolStaticAnalyser().validate(module);
 		
 		for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
-			IModel model = modelDeclaration.getModel();
-				if(model instanceof IRewriter)
-				{
-					((IRewriter)model).rewrite(module, module.getCompilationContext());
-				}
+			if (modelDeclaration.doOptimisation().equals("true")) {
+				IModel model = modelDeclaration.getModel();
+			if (modelDeclaration.getDriverNameExpression().getName().equals("MySQL")) {
+				context.setModelFactory(new SubModelFactory());
+				new MySqlQueryRewriter().rewrite(model, module, context);
 			}
+
+			if (modelDeclaration.getDriverNameExpression().getName().equals("EMF")) {
+				context.setModelFactory(new SubEmfModelFactory());
+				new EmfQueryRewriter().rewrite(model, module, context);
+			}
+			}
+		}
 		String actual = new EolUnparser().unparse(module);
 		
-		String expected = "model earth driver EMF {nsuri = \"friends\"}\n"
-				+ "var check : Boolean = true;\n"
-				+ "if (check) {\n"
-				+ "	earth.findByIndex(\"Person\", \"name\", \"Judy\", \"false\").println();\n"
-				+ "}\n"
-				+ "else {\n"
-				+ "	for (p in earth!Person.all) {\n"
-				+ "		earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "	}\n"
-				+ "}\n"
-				+ "earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "var exists : Boolean = true;\n"
-				+ "if (exists) {\n"
-				+ "	if (exists) {\n"
-				+ "		earth!Person.all.select(b : earth!Person|b.name = \"Judy\" or b.name = \"Julie\").println();\n"
-				+ "	}\n"
-				+ "	earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "}\n"
-				+ "else {\n"
-				+ "	if (exists) {\n"
-				+ "		earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "	}\n"
-				+ "	earth.findByIndex(\"Person\", \"name\", \"Julie\", \"true\").println();\n"
-				+ "}\n"
-				+ "for (p in earth!Person.all) {\n"
-				+ "	if (exists) {\n"
-				+ "		earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "	}\n"
-				+ "	earth.findByIndex(\"Person\", \"name\", \"Judy\", \"true\").println();\n"
-				+ "	earth.findByIndex(\"Person\", \"name\", \"Julie\", \"true\").println();\n"
-				+ "}\n"
-				+ "earth.findByIndex(\"Person\", \"name\", \"Monica\", \"true\").println();\n"
-				+ "earth.findByIndex(\"Person\", \"name\", \"Rachel\", \"true\").println();\n";
+		String expected = Files.readString(Path.of("src/org/eclipse/epsilon/eol/query/emfRewritedQuery.txt"));
+		
 		assertEquals("Failed", expected, actual);
 
 	}
