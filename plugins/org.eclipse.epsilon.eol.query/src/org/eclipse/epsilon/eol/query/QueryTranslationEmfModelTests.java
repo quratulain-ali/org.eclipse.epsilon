94ca6ec0def3315f439b7e38cde4b4b969281bec
quratulain-york <quratulain.ali@york.ac.uk>
2021-02-02 15:30:04 +0000

    Added testcases for call graph and rewriting

--- plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryTranslationEmfModelTests.java
+++ plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/query/QueryTranslationEmfModelTests.java
@@ -1,8 +1,12 @@
 package org.eclipse.epsilon.eol.query;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.epsilon.emc.emf.SubEmfModelFactory;
 import org.eclipse.epsilon.emc.mysql.SubModelFactory;
@@ -19,10 +23,31 @@ import junit.framework.TestCase;
 public class QueryTranslationEmfModelTests extends TestCase {
 
 	@Test
-	public static void test() throws Exception {
+	public static void testQueryRewriting() throws Exception {
+		
+		List<String> actualAndExpected = new ArrayList<>();
+		actualAndExpected = prepareTestCase("CallGraphEmfRewriterTestCase.eol", "CallGraphEmf.txt",1);
+		assertEquals("Failed", actualAndExpected.get(1), actualAndExpected.get(0));
+	}
+	
+	@Test
+	public static void testCallGraph() throws Exception {
+		
+		List<String> actualAndExpected = new ArrayList<>();
+		actualAndExpected = prepareTestCase("TestCallGraph.eol", "CallGraph.dot",2);
+		assertEquals("Failed", actualAndExpected.get(1), actualAndExpected.get(0));
+	}
+	
+	
+	public static List<String> prepareTestCase(String eolFileName, String rewritedFileName, Integer option) {
 		EolModule module = new EolModule();
 
-		module.parse(new File("src/org/eclipse/epsilon/eol/query/emfTest.eol"));
+		try {
+			module.parse(new File("src/org/eclipse/epsilon/eol/query/"+eolFileName));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         EolCompilationContext context = module.getCompilationContext();
 		
 		for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
@@ -50,12 +75,34 @@ public class QueryTranslationEmfModelTests extends TestCase {
 			}
 			}
 		}
-		String actual = new EolUnparser().unparse(module);
+		String actual = "";
+		switch(option) {
+		  case 1:
+			  actual = new EolUnparser().unparse(module);
+		    break;
+		  case 2:
+			  String pathAndFileName = "src/org/eclipse/epsilon/eol/query/generatedCallGraph.dot";
+			  staticAnlayser.exportCallGraph(pathAndFileName);
+			  try {
+				actual = Files.readString(Path.of(pathAndFileName));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		    break;
+		  default:
+		    // code block
+		}
+			
 		
-		String expected = Files.readString(Path.of("src/org/eclipse/epsilon/eol/query/emfRewritedQuery.txt"));
-		
-		assertEquals("Failed", expected, actual);
-
+		String expected = "";
+		try {
+			expected = Files.readString(Path.of("src/org/eclipse/epsilon/eol/query/"+rewritedFileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Arrays.asList(actual,expected);
 	}
 
 }
