082c2622a8cae4337b349c22ce0f31fc1693614e
quratulain-york <quratulain.ali@york.ac.uk>
2021-02-11 22:03:21 +0000

    Added EvlLaunchConfiguration for rewriting

---
+++ plugins/org.eclipse.epsilon.evl.query/src/org/eclipse/epsilon/evl/query/EvlLaunchConfiguration.java
@@ -0,0 +1,58 @@
+package org.eclipse.epsilon.evl.query;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.epsilon.eol.IEolModule;
+import org.eclipse.epsilon.eol.dom.ModelDeclaration;
+import org.eclipse.epsilon.eol.dt.launching.EpsilonLaunchConfigurationDelegateListener;
+import org.eclipse.epsilon.erl.ErlModule;
+import org.eclipse.epsilon.evl.EvlModule;
+import org.eclipse.epsilon.evl.parse.EvlUnparser;
+import org.eclipse.epsilon.evl.staticanalyser.EvlStaticAnalyser;
+
+public class EvlLaunchConfiguration implements EpsilonLaunchConfigurationDelegateListener {
+
+	public EvlLaunchConfiguration() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void aboutToParse(ILaunchConfiguration configuration, String mode, ILaunch launch,
+			IProgressMonitor progressMonitor, IEolModule module) throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void aboutToExecute(ILaunchConfiguration configuration, String mode, ILaunch launch,
+			IProgressMonitor progressMonitor, IEolModule module) throws Exception {
+//		for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
+//			if (modelDeclaration.getDriverNameExpression().getName().equals("MySQL")) 
+//				module.getCompilationContext().setModelFactory(new SubJdbcModelFactory());
+//
+//			if (modelDeclaration.getDriverNameExpression().getName().equals("EMF")) 
+//				module.getCompilationContext().setModelFactory(new SubEmfModelFactory());
+//		}
+//		
+//		if(module instanceof EvlModule)  {
+//			module = (EvlModule)module;
+//			new EvlStaticAnalyser().validate(module);
+//        
+//		new EvlRewritingHandler().invokeRewriters(module);
+//		
+//		System.err.println(new EvlUnparser().unparse((ErlModule) module));
+//		}
+			
+
+	}
+
+	@Override
+	public void executed(ILaunchConfiguration configuration, String mode, ILaunch launch,
+			IProgressMonitor progressMonitor, IEolModule module, Object result) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
