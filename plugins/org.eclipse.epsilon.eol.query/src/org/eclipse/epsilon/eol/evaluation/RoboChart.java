a7d500317455ee32bbfbb8283bb8d36a3fc09d8c
quratulain-york <quratulain.ali@york.ac.uk>
2021-02-17 20:08:29 +0000

    added testcases from github

---
+++ plugins/org.eclipse.epsilon.eol.query/src/org/eclipse/epsilon/eol/evaluation/RoboChart.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The University of York.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Contributors:
+ *     Dimitrios Kolovos - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.eol.evaluation;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.eclipse.epsilon.common.util.StringProperties;
+import org.eclipse.epsilon.eol.launch.EolRunConfiguration;
+import org.eclipse.epsilon.emc.emf.EmfModel;
+
+/**
+ * This example demonstrates using the Epsilon Object Language, the core language of Epsilon, in a stand-alone manner 
+ * 
+ * @author Sina Madani
+ * @author Dimitrios Kolovos
+ */
+public class RoboChart {
+	
+	public static void main(String[] args) throws Exception {
+		Path root = Paths.get(RoboChart.class.getResource("").toURI()),
+			modelsRoot = root.getParent().resolve("evaluation");
+		
+		StringProperties modelProperties = new StringProperties();
+		modelProperties.setProperty(EmfModel.PROPERTY_NAME, "robochart");
+		modelProperties.setProperty(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI,
+			modelsRoot.resolve("robochart.ecore").toAbsolutePath().toUri().toString()
+		);
+		modelProperties.setProperty(EmfModel.PROPERTY_MODEL_URI,
+			modelsRoot.resolve("auv.model").toAbsolutePath().toUri().toString()
+		);
+		
+		EolRunConfiguration runConfig = EolRunConfiguration.Builder()
+			.withScript(root.resolve("r3.eol"))
+			.withModel(new EmfModel(), modelProperties)
+			.withParameter("Thread", Thread.class)
+			.withProfiling()
+			.build();
+		
+		EolPreExecuteConfiguration sm = new EolPreExecuteConfiguration(runConfig);
+		sm.run();
+		System.out.println(sm.getResult());
+	}
+	
+}
